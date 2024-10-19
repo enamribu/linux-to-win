a828d122a0ada4ddcfa9640d2e6cf2ae9d354699
Nana Sulis <111860707+enamribu@users.noreply.github.com>
2024-10-19 11:32:41 +0700

    tes.sh

--- tes.sh
+++ tes.sh
@@ -12,12 +12,12 @@ echo "	6) Link gz"
 read -p "Pilih [1]: " PILIHOS
 
 case "$PILIHOS" in
-	1|"") PILIHOS="http://139.59.227.187/windows2012.gz";;
-	2) PILIHOS="http://139.59.227.187/windows2016.gz";;
-	3) PILIHOS="http://139.59.227.187/windows2019.gz";;
-	4) PILIHOS="http://139.59.227.187/windows2022.gz";;
-	5) PILIHOS="http://139.59.227.187/windows10.gz";;
-	6) read -p "Masukkan Link GZ mu : " PILIHOS;;
+	1|"") PILIHOS="http://139.59.227.187/windows2012.gz"
+	2) PILIHOS="http://139.59.227.187/windows2016.gz"
+	3) PILIHOS="http://139.59.227.187/windows2019.gz"
+	4) PILIHOS="http://139.59.227.187/windows2022.gz"
+	5) PILIHOS="http://139.59.227.187/windows10.gz"
+	6) read -p "Masukkan Link GZ mu : " PILIHOS
 	*) echo "pilihan salah"; exit;;
 esac
 
