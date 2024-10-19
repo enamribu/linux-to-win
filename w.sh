78404cbd885e613f87dd739378711d37fd850cd1
Nana Sulis <111860707+enamribu@users.noreply.github.com>
2024-10-19 14:37:55 +0700

    w.sh

--- w.sh
+++ w.sh
@@ -1,25 +1,94 @@
 #!/bin/bash
 #
+# CREATED By NIXPOIN.COM
 #
 echo "Pilih OS yang ingin anda install"
-echo "	1) Windows 2012"
+echo "	1) Windows 2019(Default)"
 echo "	2) Windows 2016"
-echo "	3) Windows 2019"
-echo "	4) Windows 2022"
-echo "	5) Windows 10"
-echo "	6) Link gz"
+echo "	3) Windows 2012"
+echo "	4) Windows 10"
+echo "	5) Windows 2022"
+echo "	6) Pakai link gz mu sendiri"
 
-read -p LINK : 
+read -p "Pilih [1]: " PILIHOS
 
-case "$LINK" in
-	1) LINK='http://139.59.227.187/windows2012.gz' ;;
-	2) LINK='http://139.59.227.187/windows2016.gz' ;;
-	3) LINK='http://139.59.227.187/windows2019.gz' ;;
-	4) LINK='http://139.59.227.187/windows2022.gz' ;;
-	5) LINK='http://139.59.227.187/windows10.gz' ;;
-	6) read -p "Masukkan Link GZ : " LINK ;;
+case "$PILIHOS" in
+	1|"") PILIHOS="https://files.sowan.my.id/windows2019.gz"  IFACE="Ethernet Instance 0 2";;
+	2) PILIHOS="https://files.sowan.my.id/windows2016.gz"  IFACE="Ethernet Instance 0 2";;
+	3) PILIHOS="https://files.sowan.my.id/windows2012.gz"  IFACE="Ethernet";;
+	4) PILIHOS="https://files.sowan.my.id/windows10.gz"  IFACE="Ethernet Instance 0 2";;
+	5) PILIHOS="https://files.sowan.my.id/windows2022.gz"  IFACE="Ethernet Instance 0 2";;
+	6) read -p "Masukkan Link GZ mu : " PILIHOS;;
+	*) echo "pilihan salah"; exit;;
 esac
 
-wget -O reinstall.sh https://raw.githubusercontent.com/bin456789/reinstall/refs/heads/main/reinstall.sh && bash reinstall.sh dd --img $LINK
+echo "Merasa terbantu dengan script ini? Anda bisa memberikan dukungan melalui QRIS kami https://nixpoin.com/qris"
 
-reboot
+read -p "Masukkan password untuk akun Administrator (minimal 12 karakter): " PASSADMIN
+
+IP4=$(curl -4 -s icanhazip.com)
+GW=$(ip route | awk '/default/ { print $3 }')
+
+
+cat >/tmp/net.bat<<EOF
+@ECHO OFF
+cd.>%windir%\GetAdmin
+if exist %windir%\GetAdmin (del /f /q "%windir%\GetAdmin") else (
+echo CreateObject^("Shell.Application"^).ShellExecute "%~s0", "%*", "", "runas", 1 >> "%temp%\Admin.vbs"
+"%temp%\Admin.vbs"
+del /f /q "%temp%\Admin.vbs"
+exit /b 2)
+net user Administrator $PASSADMIN
+
+
+netsh -c interface ip set address name="$IFACE" source=static address=$IP4 mask=255.255.240.0 gateway=$GW
+netsh -c interface ip add dnsservers name="$IFACE" address=1.1.1.1 index=1 validate=no
+netsh -c interface ip add dnsservers name="$IFACE" address=8.8.4.4 index=2 validate=no
+
+cd /d "%ProgramData%/Microsoft/Windows/Start Menu/Programs/Startup"
+del /f /q net.bat
+exit
+EOF
+
+
+cat >/tmp/dpart.bat<<EOF
+@ECHO OFF
+echo JENDELA INI JANGAN DITUTUP
+echo SCRIPT INI AKAN MERUBAH PORT RDP MENJADI 3389, SETELAH RESTART UNTUK MENYAMBUNG KE RDP GUNAKAN ALAMAT $IP4:3389
+echo KETIK YES LALU ENTER!
+
+cd.>%windir%\GetAdmin
+if exist %windir%\GetAdmin (del /f /q "%windir%\GetAdmin") else (
+echo CreateObject^("Shell.Application"^).ShellExecute "%~s0", "%*", "", "runas", 1 >> "%temp%\Admin.vbs"
+"%temp%\Admin.vbs"
+del /f /q "%temp%\Admin.vbs"
+exit /b 2)
+
+set PORT=3389
+set RULE_NAME="Open Port %PORT%"
+
+netsh advfirewall firewall show rule name=%RULE_NAME% >nul
+if not ERRORLEVEL 1 (
+    rem Rule %RULE_NAME% already exists.
+    echo Hey, you already got a out rule by that name, you cannot put another one in!
+) else (
+    echo Rule %RULE_NAME% does not exist. Creating...
+    netsh advfirewall firewall add rule name=%RULE_NAME% dir=in action=allow protocol=TCP localport=%PORT%
+)
+
+reg add "HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v PortNumber /t REG_DWORD /d 3389
+
+ECHO SELECT VOLUME=%%SystemDrive%% > "%SystemDrive%\diskpart.extend"
+ECHO EXTEND >> "%SystemDrive%\diskpart.extend"
+START /WAIT DISKPART /S "%SystemDrive%\diskpart.extend"
+
+del /f /q "%SystemDrive%\diskpart.extend"
+cd /d "%ProgramData%/Microsoft/Windows/Start Menu/Programs/Startup"
+del /f /q dpart.bat
+timeout 50 >nul
+del /f /q ChromeSetup.exe
+echo JENDELA INI JANGAN DITUTUP
+exit
+EOF
+
+wget  -O reinstall.sh 'https://raw.githubusercontent.com/bin456789/reinstall/refs/heads/main/reinstall.sh' && bash reinstall.sh dd --img $PILIHOS
