8138390143a90726898e18b8885664299969e85c
Nana Sulis <111860707+enamribu@users.noreply.github.com>
2024-10-19 12:03:13 +0700

    tes.sh

--- tes.sh
+++ tes.sh
@@ -1,28 +1,21 @@
-#!/bin/bash
-#
-#
-echo "Pilih OS yang ingin anda install"
-echo "	1) Windows 2012"
-echo "	2) Windows 2016"
-echo "	3) Windows 2019"
-echo "	4) Windows 2022"
-echo "	5) Windows 10"
-echo "	6) Link gz"
+#!/bin/sh
 
-read -p PILIHOS
+wget  -O k.sh 'https://raw.githubusercontent.com/bin456789/reinstall/refs/heads/main/reinstall.sh'
 
-case "$PILIHOS" in
-	1) PILIHOS="http://139.59.227.187/windows2012.gz" ;;
-	2) #PILIHOS="http://139.59.227.187/windows2016.gz" ;;
-	3) PILIHOS="http://139.59.227.187/windows2019.gz" ;;
-	4) PILIHOS="http://139.59.227.187/windows2022.gz" ;;
-	5) PILIHOS="http://139.59.227.187/windows10.gz" ;;
-	6) read -p "Masukkan Link GZ mu : " PILIHOS ;;
-	*) echo "pilihan salah"; exit;;
+echo "  1) Windows 2012"
+echo "  2) Windows 2016"
+echo "  3) Windows 2019"
+echo "  4) Windows 2022"
+echo "  5) Windows 10"
+echo "  6) Link GZ"
+echo -n "Masukkan Pilihan: "
+read N
+case $N in
+  1) bash k.sh dd --img 'http://139.59.227.187/windows2012.gz' ;;
+  2) bash k.sh dd --img 'http://139.59.227.187/windows2016.gz' ;;
+  3) bash k.sh dd --img 'http://139.59.227.187/windows2019.gz' ;;
+  4) bash k.sh dd --img 'http://139.59.227.187/windows2022.gz' ;;
+  5) bash k.sh dd --img 'http://139.59.227.187/windows10.gz' ;;
+  6) bash k.sh dd --img ;;
+  *) echo "Wrong input!" ;;
 esac
-
-wget -O k.sh https://raw.githubusercontent.com/bin456789/reinstall/refs/heads/main/reinstall.sh && bash k.sh dd --img $PILIHOS
-
-echo 'Your server will reboot in 3 second'
-sleep 3
-reboot
