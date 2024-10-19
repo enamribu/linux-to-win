98dd9ea56f4122be18fdd31ace561f06f4b179b6
Nana Sulis <111860707+enamribu@users.noreply.github.com>
2024-10-19 14:42:25 +0700

    w.sh

--- w.sh
+++ w.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#
-# CREATED By NIXPOIN.COM
-#
+
 echo "Pilih OS yang ingin anda install"
 echo "	1) Windows 2019(Default)"
 echo "	2) Windows 2016"
@@ -21,30 +19,5 @@ case "$PILIHOS" in
 	6) read -p "Masukkan Link GZ mu : " PILIHOS;;
 	*) echo "pilihan salah"; exit;;
 esac
-
-
-IP4=$(curl -4 -s icanhazip.com)
-GW=$(ip route | awk '/default/ { print $3 }')
-
-
-cat >/tmp/net.bat<<EOF
-@ECHO OFF
-cd.>%windir%\GetAdmin
-if exist %windir%\GetAdmin (del /f /q "%windir%\GetAdmin") else (
-echo CreateObject^("Shell.Application"^).ShellExecute "%~s0", "%*", "", "runas", 1 >> "%temp%\Admin.vbs"
-"%temp%\Admin.vbs"
-del /f /q "%temp%\Admin.vbs"
-exit /b 2)
-net user Administrator $PASSADMIN
-
-
-netsh -c interface ip set address name="$IFACE" source=static address=$IP4 mask=255.255.240.0 gateway=$GW
-netsh -c interface ip add dnsservers name="$IFACE" address=1.1.1.1 index=1 validate=no
-netsh -c interface ip add dnsservers name="$IFACE" address=8.8.4.4 index=2 validate=no
-
-cd /d "%ProgramData%/Microsoft/Windows/Start Menu/Programs/Startup"
-del /f /q net.bat
-exit
-EOF
 
 wget  -O reinstall.sh 'https://raw.githubusercontent.com/bin456789/reinstall/refs/heads/main/reinstall.sh' && bash reinstall.sh dd --img $PILIHOS
