63ea47eb28e8e33aa1f082bba0d68a3a3729c77a
Nana Sulis <111860707+enamribu@users.noreply.github.com>
2024-10-19 13:32:39 +0700

    tes.sh

--- tes.sh
+++ tes.sh
@@ -7,7 +7,7 @@ echo "  2) Windows 2016"
 echo "  3) Windows 2019"
 echo "  4) Windows 2022"
 echo "  5) Windows 10"
-echo "  6) Link GZ"
+echo "  6) Windows 10 US"
 echo -n "Masukkan Pilihan: "
 read N
 case $N in
@@ -16,7 +16,7 @@ case $N in
   3) bash reinstall.sh dd --img 'http://139.59.227.187/windows2019.gz' ;;
   4) bash reinstall.sh dd --img 'http://139.59.227.187/windows2022.gz' ;;
   5) bash reinstall.sh dd --img 'http://139.59.227.187/windows10.gz' ;;
-  6) bash reinstall.sh dd --img 'Masukkan Link' ;;
+  6) bash reinstall.sh dd --img 'https://download1529.mediafire.com/hqfm3ogkl8sguOSI-wQ-O4lRJ6xPWfr5v7wKuh8XYm0sHv2jQ07Kej1T4X0heImIYONXFxo0oErQZYSzHd89rCiZWPRO-bpI1tsvtUDVJrAi4kJKs7UmihewZjTCIZ6Zcxj66jmuEA8OM3YfKQMSaVyn-hFTUsvvAJzUJ_Vgx6VO/6rzrtbg7ity8gkh/windows10.gz' ;;
   *) echo "Wrong input!" ;;
 esac
 reboot
