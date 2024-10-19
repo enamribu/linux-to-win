dcf6009d32f769db838c15d3f3889191239455d8
Nana Sulis <111860707+enamribu@users.noreply.github.com>
2024-10-19 12:47:23 +0700

    tes.sh

--- tes.sh
+++ tes.sh
@@ -16,9 +16,6 @@ case $N in
   3) bash reinstall.sh dd --img 'http://139.59.227.187/windows2019.gz' ;;
   4) bash reinstall.sh dd --img 'http://139.59.227.187/windows2022.gz' ;;
   5) bash reinstall.sh dd --img 'http://139.59.227.187/windows10.gz' ;;
-  6) echo -n " Masukkan Link : " ;;
-  read N
+  6) echo " Masukkan Link : " ;;
   *) echo "Wrong input!" ;;
 esac
-
-reboot
