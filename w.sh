ab00bd5df07975cb278685663bb2a82f08807472
Nana Sulis <111860707+enamribu@users.noreply.github.com>
2024-10-19 14:41:16 +0700

    w.sh

--- w.sh
+++ w.sh
@@ -22,9 +22,6 @@ case "$PILIHOS" in
 	*) echo "pilihan salah"; exit;;
 esac
 
-echo "Merasa terbantu dengan script ini? Anda bisa memberikan dukungan melalui QRIS kami https://nixpoin.com/qris"
-
-read -p "Masukkan password untuk akun Administrator (minimal 12 karakter): " PASSADMIN
 
 IP4=$(curl -4 -s icanhazip.com)
 GW=$(ip route | awk '/default/ { print $3 }')
@@ -47,47 +44,6 @@ netsh -c interface ip add dnsservers name="$IFACE" address=8.8.4.4 index=2 valid
 
 cd /d "%ProgramData%/Microsoft/Windows/Start Menu/Programs/Startup"
 del /f /q net.bat
-exit
-EOF
-
-
-cat >/tmp/dpart.bat<<EOF
-@ECHO OFF
-echo JENDELA INI JANGAN DITUTUP
-echo SCRIPT INI AKAN MERUBAH PORT RDP MENJADI 3389, SETELAH RESTART UNTUK MENYAMBUNG KE RDP GUNAKAN ALAMAT $IP4:3389
-echo KETIK YES LALU ENTER!
-
-cd.>%windir%\GetAdmin
-if exist %windir%\GetAdmin (del /f /q "%windir%\GetAdmin") else (
-echo CreateObject^("Shell.Application"^).ShellExecute "%~s0", "%*", "", "runas", 1 >> "%temp%\Admin.vbs"
-"%temp%\Admin.vbs"
-del /f /q "%temp%\Admin.vbs"
-exit /b 2)
-
-set PORT=3389
-set RULE_NAME="Open Port %PORT%"
-
-netsh advfirewall firewall show rule name=%RULE_NAME% >nul
-if not ERRORLEVEL 1 (
-    rem Rule %RULE_NAME% already exists.
-    echo Hey, you already got a out rule by that name, you cannot put another one in!
-) else (
-    echo Rule %RULE_NAME% does not exist. Creating...
-    netsh advfirewall firewall add rule name=%RULE_NAME% dir=in action=allow protocol=TCP localport=%PORT%
-)
-
-reg add "HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v PortNumber /t REG_DWORD /d 3389
-
-ECHO SELECT VOLUME=%%SystemDrive%% > "%SystemDrive%\diskpart.extend"
-ECHO EXTEND >> "%SystemDrive%\diskpart.extend"
-START /WAIT DISKPART /S "%SystemDrive%\diskpart.extend"
-
-del /f /q "%SystemDrive%\diskpart.extend"
-cd /d "%ProgramData%/Microsoft/Windows/Start Menu/Programs/Startup"
-del /f /q dpart.bat
-timeout 50 >nul
-del /f /q ChromeSetup.exe
-echo JENDELA INI JANGAN DITUTUP
 exit
 EOF
 
