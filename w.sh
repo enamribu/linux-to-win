e4eecc4449c99023eafc6ff6d13a8bfbc059ab3a
Nana Sulis <111860707+enamribu@users.noreply.github.com>
2024-10-19 15:01:43 +0700

    w.sh

--- w.sh
+++ w.sh
@@ -6,7 +6,7 @@ echo "	2) Windows 2016"
 echo "	3) Windows 2012"
 echo "	4) Windows 10"
 echo "	5) Windows 2022"
-echo "	6) Pakai link gz mu sendiri"
+echo "	6) Link GZ"
 
 read -p "Pilih [1]: " PILIHOS
 
@@ -16,7 +16,7 @@ case "$PILIHOS" in
 	3) PILIHOS="http://139.59.227.187/windows2012.gz"  IFACE="Ethernet";;
 	4) PILIHOS="http://139.59.227.187/windows10.gz"  IFACE="Ethernet Instance 0 2";;
 	5) PILIHOS="http://139.59.227.187/windows2022.gz"  IFACE="Ethernet Instance 0 2";;
-	6) read -p "Masukkan Link GZ mu : " PILIHOS;;
+	6) read -p "Masukkan Link GZ : " PILIHOS;;
 	*) echo "pilihan salah"; exit;;
 esac
 
