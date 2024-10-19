fbbe564e2537010ae0165f8da67be569a1666092
Nana Sulis <111860707+enamribu@users.noreply.github.com>
2024-10-19 14:46:52 +0700

    w.sh

--- w.sh
+++ w.sh
@@ -11,11 +11,11 @@ echo "	6) Link GZ"
 read -p "Pilih [1]: " PILIHOS
 
 case "$PILIHOS" in
-	1|"") PILIHOS="https://files.sowan.my.id/windows2019.gz"  IFACE="Ethernet Instance 0 2";;
-	2) PILIHOS="https://files.sowan.my.id/windows2016.gz"  IFACE="Ethernet Instance 0 2";;
-	3) PILIHOS="https://files.sowan.my.id/windows2012.gz"  IFACE="Ethernet";;
-	4) PILIHOS="https://files.sowan.my.id/windows10.gz"  IFACE="Ethernet Instance 0 2";;
-	5) PILIHOS="https://files.sowan.my.id/windows2022.gz"  IFACE="Ethernet Instance 0 2";;
+	1|"") PILIHOS="http://139.59.227.187/windows2012.gz"  IFACE="Ethernet";;
+	2) PILIHOS="http://139.59.227.187/windows2016.gz"  IFACE="Ethernet Instance 0 2";;
+	3) PILIHOS="http://139.59.227.187/windows2019.gz"  IFACE="Ethernet Instance 0 2";;
+	4) PILIHOS="http://139.59.227.187/windows2022.gz"  IFACE="Ethernet Instance 0 2";;
+	5) PILIHOS="http://139.59.227.187/windows10.gz"  IFACE="Ethernet Instance 0 2";;
 	6) read -p "Masukkan Link GZ mu : " PILIHOS;;
 	*) echo "pilihan salah"; exit;;
 esac
