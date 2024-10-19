e5948be7f9e3994b02dbde9606edff5c9459f9ef
Nana Sulis <111860707+enamribu@users.noreply.github.com>
2024-10-19 18:16:16 +0700

    Update t.sh

--- t.sh
+++ t.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "Pilih OS yang ingin anda install"
-echo "	1) Windows 2012(Default)"
+echo "	1) Windows 2012"
 echo "	2) Windows 2016"
 echo "	3) Windows 2019"
 echo "	4) Windows 2022"
 echo "	5) Windows 10"
 echo "	6) Link GZ"
 
-read -p "Pilih [1]: " PILIHOS
+read -p "Pilih : " PILIHOS
 
 case "$PILIHOS" in
 	1|"") PILIHOS="http://139.59.227.187/windows2012.gz"  IFACE="Ethernet Instance 0 2";;
