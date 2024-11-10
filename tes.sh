974fa49d1b48b49a94f6ee9b73f9a33c856febdc
Nana Sulis <111860707+enamribu@users.noreply.github.com>
2024-11-10 08:53:24 +0700

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -1,22 +1,37 @@
-#!/bin/sh
+#!/bin/bash
 
-wget  -O reinstall.sh 'https://raw.githubusercontent.com/bin456789/reinstall/refs/heads/main/reinstall.sh'
+echo "Pilih OS yang ingin anda install"
+echo "	1) Windows 2012"
+echo "	2) Windows 2016"
+echo "	3) Windows 2019"
+echo "	4) Windows 2022"
+echo "	5) Windows 10"
+echo "	6) Link GZ"
 
-echo "  1) Windows 2012"
-echo "  2) Windows 2016"
-echo "  3) Windows 2019"
-echo "  4) Windows 2022"
-echo "  5) Windows 10"
-echo "  6) Windows 10 US"
-echo -n "Masukkan Pilihan: "
-read N
-case $N in
-  1) bash reinstall.sh dd --img 'http://139.59.227.187/windows2012.gz' ;;
-  2) bash reinstall.sh dd --img 'http://139.59.227.187/windows2016.gz' ;;
-  3) bash reinstall.sh dd --img 'http://139.59.227.187/windows2019.gz' ;;
-  4) bash reinstall.sh dd --img 'http://139.59.227.187/windows2022.gz' ;;
-  5) bash reinstall.sh dd --img 'http://139.59.227.187/windows10.gz' ;;
-  6) bash reinstall.sh dd --img 'https://download1529.mediafire.com/hqfm3ogkl8sguOSI-wQ-O4lRJ6xPWfr5v7wKuh8XYm0sHv2jQ07Kej1T4X0heImIYONXFxo0oErQZYSzHd89rCiZWPRO-bpI1tsvtUDVJrAi4kJKs7UmihewZjTCIZ6Zcxj66jmuEA8OM3YfKQMSaVyn-hFTUsvvAJzUJ_Vgx6VO/6rzrtbg7ity8gkh/windows10.gz' ;;
-  *) echo "Wrong input!" ;;
+read -p "Pilih : " PILIHOS
+
+case "$PILIHOS" in
+	1|"") PILIHOS="http://139.59.227.187/windows2012.gz"  IFACE="Ethernet Instance 0 2";;
+	2) PILIHOS="http://139.59.227.187/windows2016.gz"  IFACE="Ethernet Instance 0 2";;
+	3) PILIHOS="http://139.59.227.187/windows2019.gz"  IFACE="Ethernet";;
+	4) PILIHOS="http://139.59.227.187/windows2022.gz"  IFACE="Ethernet Instance 0 2";;
+	5) PILIHOS="http://139.59.227.187/windows10.gz"  IFACE="Ethernet Instance 0 2";;
+	6) read -p "Masukkan Link GZ : " PILIHOS;;
+	*) echo "pilihan salah"; exit;;
 esac
+
+echo "Pakai port atau tidak"
+echo "	1) CUSTOM PORT"
+echo "	2) TIDAK"
+
+read -p "Pilih : " PORT
+
+case "$PILIHOS" in
+	1|"") read -p "Masukkan PORT : " PORT;;
+	2) wget  -O reinstall.sh 'https://raw.githubusercontent.com/bin456789/reinstall/refs/heads/main/reinstall.sh' && bash reinstall.sh dd --img $PILIHOS
+	*) echo "pilihan salah"; exit;;
+esac
+
+wget  -O reinstall.sh 'https://raw.githubusercontent.com/bin456789/reinstall/refs/heads/main/reinstall.sh' && bash reinstall.sh dd --img $PILIHOS --rdp-port $PORT
+
 reboot
