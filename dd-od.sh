61298eaf855c495ba056affadb3224bb32ed901b
Nana Sulis <111860707+enamribu@users.noreply.github.com>
2025-02-04 17:03:20 +0700

    Update dd-od.sh

--- dd-od.sh
+++ dd-od.sh
@@ -1,90 +1,58 @@
-#!/bin/sh
+#!/bin/bash
 
+# Pastikan script dijalankan dengan hak akses root
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Script ini harus dijalankan dengan hak akses root!"
+  exit 1
+fi
 
-sh_ver="1.0.2"
+# Tentukan lokasi file .gz yang berisi image Windows (IMG)
+GZ_FILE="/root/windows.gz"
+# Tentukan lokasi disk tujuan (misalnya USB /dev/sdX)
+TARGET_DISK="/dev/vda"
 
+# Mengecek apakah file .gz ada
+if [ ! -f "$GZ_FILE" ]; then
+  echo "File $GZ_FILE tidak ditemukan!"
+  exit 1
+fi
 
+# Mengecek apakah disk target sudah benar
+if [ ! -b "$TARGET_DISK" ]; then
+  echo "Disk tujuan $TARGET_DISK tidak ditemukan!"
+  exit 1
+fi
 
+# Membuat direktori untuk ekstraksi sementara
+EXTRACT_DIR="/tmp/windows_extract"
+mkdir -p $EXTRACT_DIR
 
-#Upgrade Script
-Update_Shell(){
-	sh_new_ver=$(wget --no-check-certificate -qO- -t1 -T3 "https://raw.githubusercontent.com/veip007/hj/master/hj.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1) && sh_new_type="github"
-	[[ -z ${sh_new_ver} ]] && echo -e "${Error} Unable to link to Github!" && exit 0
-	wget -N --no-check-certificate "https://raw.githubusercontent.com/nanasulis/linux-to-win/main/dd-od.sh" && chmod +x dd-od.sh
-	echo -e "The script has been updated to the latest version[ ${sh_new_ver} ] !(Note: Because the update method is to directly overwrite the currently running script, some errors may be prompted below, just ignore them)" && exit 0
-}
+# Mengekstrak file .gz untuk mendapatkan file .img
+echo "Mengekstrak file $GZ_FILE menjadi file .img..."
+tar -xzvf "$GZ_FILE" -C "$EXTRACT_DIR"
 
+# Mencari file .img setelah ekstraksi
+IMG_FILE=$(find "$EXTRACT_DIR" -name "*.img" -type f)
 
+if [ -z "$IMG_FILE" ]; then
+  echo "Tidak ada file .img ditemukan setelah ekstraksi!"
+  exit 1
+fi
 
+echo "File .img ditemukan: $IMG_FILE"
 
-MAINIP=$(ip route get 1 | awk '{print $NF;exit}')
-GATEWAYIP=$(ip route | grep default | awk '{print $3}')
-SUBNET=$(ip -o -f inet addr show | awk '/scope global/{sub(/[^.]+\//,"0/",$4);print $4}' | head -1 | awk -F '/' '{print $2}')
+# Memberi peringatan agar disk target tidak memiliki data penting
+echo "Peringatan: Semua data di $TARGET_DISK akan terhapus. Pastikan Anda sudah melakukan backup!"
 
-value=$(( 0xffffffff ^ ((1 << (32 - $SUBNET)) - 1) ))
-NETMASK="$(( (value >> 24) & 0xff )).$(( (value >> 16) & 0xff )).$(( (value >> 8) & 0xff )).$(( value & 0xff ))"
+# Menulis file .img ke disk tujuan
+echo "Menulis file .img ke disk $TARGET_DISK..."
+dd if="$IMG_FILE" of="$TARGET_DISK" bs=4M status=progress
 
-wget --no-check-certificate -qO InstallNET.sh 'https://raw.githubusercontent.com/nanasulis/linux-to-win/main/InstallNET.sh' && chmod a+x InstallNET.sh && wget --no-check-certificate -qO Network-Reinstall-System-Modify.sh 'https://raw.githubusercontent.com/nanasulis/linux-to-win/main/Network-Reinstall-System-Modify.sh' && chmod a+x Network-Reinstall-System-Modify.sh
+# Menunggu proses selesai
+sync
 
-clear
-echo "                                                              "
-echo "##############################################################"
-echo "#                                                            #"
-echo "#  Auto DD                                                   #"
-echo "#                                                            #"
-echo "#  Last Modified: 2024-07-24                                 #"
-echo "#  Linux默认密码：MoeClub.org  or  cxthhhhh.com  or  nat.ee  #"
-echo "#  Supported by MoeClub                                      #"
-echo "#                                                            #"
-echo "##############################################################"
-echo "                                                              "
-echo "IP: $MAINIP"
-echo "网关: $GATEWAYIP"
-echo "网络掩码: $NETMASK"
-echo ""
-echo "请选择您需要的镜像包:"
-echo "  0) 升级本脚本"
-echo "  1) CentOS 7 (DD) 用户名：root 密码：Pwd@CentOS"
-echo "  2) CentOS 6 (阿里云镜像) 用户名：root 密码：MoeClub.org"
-echo "  3) CentOS 6 用户名：root 密码：MoeClub.org"
-echo "  4) Debian 7 x32 用户名：root 密码：MoeClub.org"
-echo "  5) Debian 8 x64 用户名：root 密码：MoeClub.org"
-echo "  6) Debian 9 x64 用户名：root 密码：MoeClub.org"
-echo "  7) Debian 10 x64 用户名：root 密码：cxthhhhh.com"
-echo "  8) Ubuntu 14.04x64 用户名：root 密码：MoeClub.org"
-echo "  9) Ubuntu 16.04x64 用户名：root 密码：MoeClub.org"
-echo "  10) Ubuntu 18.04x64 用户名：root 密码：MoeClub.org"
-echo "  11) 萌咖Win7x64 用户名:Administrator  密码：Vicer"
-echo "  12) Win2019 By:MeowLove  密码：cxthhhhh.com"
-echo "  13) Win2016 By:MeowLove  密码：cxthhhhh.com"
-echo "  14) Win2012 R2 By:MeowLove  密码：cxthhhhh.com"
-echo "  15) Win2008 R2 By:MeowLove  密码：cxthhhhh.com"
-echo "  16) Windows 7 Vienna By:MeowLove  密码：cxthhhhh.com"
-echo "  17) Windows 2003 Vienna By:MeowLove  密码：cxthhhhh.com"
-echo "  18) OD备份其它win镜像包链接限制，请使用gd版"
-echo "  自定义安装请使用：bash InstallNET.sh -dd '您的直连'"
-echo ""
-echo -n "请输入编号: "
-read N
-case $N in
-  0) wget -N --no-check-certificate "https://raw.githubusercontent.com/veip007/dd/master/dd-gd.sh" && chmod +x dd-gd.sh ;;
-  1) echo "Password: Pwd@CentOS" ; read -s -n1 -p "Press any key to continue..." ; bash InstallNET.sh --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK -dd 'https://api.moetools.net/get/centos-7-image' ;;
-  2) bash InstallNET.sh -c 6.9 -v 64 -a --mirror 'http://mirrors.aliyun.com/centos-vault' --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $SUBNET ;;
-  3) bash InstallNET.sh -c 6.9 -v 64 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK ;;
-  4) bash InstallNET.sh -d 7 -v 32 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK ;;
-  5) bash InstallNET.sh -d 8 -v 64 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK ;;
-  6) bash InstallNET.sh -d 9 -v 64 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK ;;
-  7) bash Network-Reinstall-System-Modify.sh -Debian_10 ;;
-  8) bash InstallNET.sh -u trusty -v 64 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK ;;
-  9) bash InstallNET.sh -u xenial -v 64 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK ;;
-  10) bash InstallNET.sh -u bionic -v 64 -a --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK ;;
-  11) bash InstallNET.sh --ip-addr $MAINIP --ip-gate $GATEWAYIP --ip-mask $NETMASK -dd 'https://微软.云.eu.org/veip=om=admin_veip_onmicrosoft_com=EUXeAeHaWcxHoA34etRizncB8gCA-hOloXzFGcGpLPPuqg';;
-  12) bash InstallNET.sh -dd https://www.dropbox.com/scl/fi/to16xl0zvxjkkium6lukx/windows2019.gz?rlkey=jdpq30d015fu7ma5nfkz0sf54&st=sr2248mt&dl=1 ;;
-  13) bash InstallNET.sh -dd https://微软.云.eu.org/veip=om=admin_veip_onmicrosoft_com=ETddFJAGxZ1Apv3-NzeT1AkBTq41BTDKVhISmotNEonkWA ;;
-  14) bash InstallNET.sh -dd https://微软.云.eu.org/veip=om=admin_veip_onmicrosoft_com=Ea01H7hU6yZDlvl4HfTx8KoB5JfvameTZVgxF6YD_SgcOQ ;;
-  15) bash InstallNET.sh -dd https://微软.云.eu.org/veip=om=admin_veip_onmicrosoft_com=EWzpQPJJBG1ImnOlXThuP3ABLbKD3mdP3_UTuV5sFBff0w ;;
-  16) bash InstallNET.sh -dd https://微软.云.eu.org/veip=om=admin_veip_onmicrosoft_com=EUXeAeHaWcxHoA34etRizncB8gCA-hOloXzFGcGpLPPuqg ;;
-  17) bash InstallNET.sh -dd https://微软.云.eu.org/veip=om=admin_veip_onmicrosoft_com=EdaMoFQWmDBMrO-9EOQBgo0BgrzVFIdBtX3rKA7bRH0-7w ;;
-  18) wget -N --no-check-certificate "https://raw.githubusercontent.com/veip007/dd/master/dd-gd.sh" && chmod +x dd-gd.sh && ./dd-gd.sh ;;
-  *) echo "Wrong input!" ;;
-esac
+echo "Proses selesai. Sistem Windows sekarang telah dipasang ke disk $TARGET_DISK."
+echo "Silakan reboot dan boot dari disk untuk mengakses Windows."
+
+# Menampilkan petunjuk untuk reboot dan install
+echo "Reboot dan pastikan untuk booting dari disk tersebut untuk melanjutkan proses instalasi Windows."
