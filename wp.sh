a0f349a890b1db2b3c4d611eed9f462f2d50e99c
Nana Sulis <111860707+enamribu@users.noreply.github.com>
2025-03-08 15:34:49 +0700

    Update wp.sh

--- wp.sh
+++ wp.sh
@@ -6,17 +6,19 @@ echo "	2) Windows 2016"
 echo "	3) Windows 2019"
 echo "	4) Windows 2022"
 echo "	5) Windows 10"
-echo "	6) Link GZ"
+echo "	6) Windows 11"
+echo "	7) Link GZ"
 
 read -p "Pilih : " PILIHOS
 
 case "$PILIHOS" in
-	1|"") PILIHOS="http://139.59.227.187/windows2012.gz"  IFACE="Ethernet Instance 0 2";;
-	2) PILIHOS="http://139.59.227.187/windows2016.gz"  IFACE="Ethernet Instance 0 2";;
-	3) PILIHOS="http://139.59.227.187/windows2019.gz"  IFACE="Ethernet";;
-	4) PILIHOS="http://139.59.227.187/windows2022.gz"  IFACE="Ethernet Instance 0 2";;
-	5) PILIHOS="http://139.59.227.187/windows10.gz"  IFACE="Ethernet Instance 0 2";;
-	6) read -p "Masukkan Link GZ : " PILIHOS;;
+	1|"") PILIHOS="http://165.227.125.198/windows2012.gz"  IFACE="Ethernet Instance 0 2";;
+	2) PILIHOS="http://165.227.125.198/windows2016.gz"  IFACE="Ethernet Instance 0 2";;
+	3) PILIHOS="http://165.227.125.198/windows2019.gz"  IFACE="Ethernet";;
+	4) PILIHOS="http://165.227.125.198/windows2022.gz"  IFACE="Ethernet Instance 0 2";;
+	5) PILIHOS="http://165.227.125.198/windows10.gz"  IFACE="Ethernet Instance 0 2";;
+ 	6) PILIHOS="http://165.227.125.198/windows11.gz"  IFACE="Ethernet Instance 0 2";;
+	7) read -p "Masukkan Link GZ : " PILIHOS;;
 	*) echo "Pilihan salah"; exit;;
 esac
 
